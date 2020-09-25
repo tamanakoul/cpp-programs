44781157acd8f2304487be1b1ae55ca944a145c4
tamanakoul <70472563+tamanakoul@users.noreply.github.com>
2020-09-25 10:58:35 +0530

    Add files via upload

---
+++ continue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+   for (int num=0; num<=6; num++) {
+     
+      if (num==3) {
+          continue;
+      }
+      cout<<num<<" ";
+   }
+   return 0;
+}
+
