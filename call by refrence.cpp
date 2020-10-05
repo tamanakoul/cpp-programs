ab128009f4a368bd969a55e83e830e0a12e19ed4
tamanakoul <70472563+tamanakoul@users.noreply.github.com>
2020-10-05 13:08:17 +0530

    Add files via upload

---
+++ call by refrence.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// function declaration
+void swap(int *x, int *y);
+
+int main () {
+   // local variable declaration:
+   int a = 100;
+   int b = 200;
+ 
+   cout << "Before swap, value of a :" << a << endl;
+   cout << "Before swap, value of b :" << b << endl;
+
+   /* calling a function to swap the values using variable reference.*/
+   swap(a, b);
+
+   cout << "After swap, value of a :" << a << endl;
+   cout << "After swap, value of b :" << b << endl;
+ 
+   return 0;
+}
+
