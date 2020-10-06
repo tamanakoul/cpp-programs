40db89bae239449a1c9df76d92c70a8802b295e3
tamanakoul <70472563+tamanakoul@users.noreply.github.com>
2020-10-06 21:50:45 +0530

    Add files via upload

---
+++ function inline.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+inline int area(int a, int b) 
+{ 
+    return a*b; 
+} 
+int main() 
+{ 
+    cout << "The area of 2,3 is: " << area(2,3) << "\n"; 
+    return 0; 
+}
